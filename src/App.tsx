97ffbb1c90b7377489414896c24ea6f73804c610
ShivaBhattacharjee <shivaxwarrior@gmail.com>
2024-07-23 15:58:53 +0530

    add: routing and error 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
-import Navbar from './components/navbar';
-import Body from './components/body';
-import Footer from './components/footer';
-
+import React from "react";
+import Navbar from "./components/navbar";
+import Body from "./components/body";
+import Footer from "./components/footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NotFound from "./pages/NotFound";
 const App: React.FC = () => {
   return (
-    <div className=' overflow-hidden'>
-      <Navbar />
-      <Body/>
-      <Footer/>
-    </div>
+    <BrowserRouter>
+      <div className=" overflow-hidden">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Body />} />
+          <Route path="/*" element={<NotFound/>} />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
