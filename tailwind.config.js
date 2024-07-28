dcebab691690c2d6f6233d3a8a144c7da4789d81
shivabhattacharjee <itsshivahere@outlook.com>
2024-07-28 17:33:42 +0530

    border beam working

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,11 +13,17 @@ module.exports = {
       animation: {
         marquee: "marquee var(--duration) linear infinite",
         "marquee-vertical": "marquee-vertical var(--duration) linear infinite",
+        "border-beam": "border-beam calc(var(--duration)*1s) infinite linear",
       },
       keyframes: {
         marquee: {
           from: { transform: "translateX(0)" },
           to: { transform: "translateX(calc(-100% - var(--gap)))" },
+        },
+        "border-beam": {
+          "100%": {
+            "offset-distance": "100%",
+          },
         },
         "marquee-vertical": {
           from: { transform: "translateY(0)" },
