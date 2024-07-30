36010a7508dadb633b0739a425186eab2820f3b0
shivabhattacharjee <itsshivahere@outlook.com>
2024-07-30 22:33:17 +0530

    completed hr page

--- src/pages/hr.tsx
+++ src/pages/hr.tsx
@@ -6,16 +6,13 @@ import {
   Plus,
   TestTube,
 } from "lucide-react";
-import hrbg from "../Images/undraw_job_offers_re_634p-green.svg";
-import charts from "../Images/charts-green.svg";
-import flow_chart from "../Images/undraw_bear_market_ania-green.svg";
-import designation from "../Images/undraw_post_re_mtr4-green.svg";
-import tasks from "../Images/undraw_add_tasks_re_s5yj-green.svg";
-import events from "../Images/undraw_events_re_98ue-green.svg";
-import vault from "../Images/undraw_vault_re_s4my-green.svg";
-import group_chat from "../Images/undraw_group_chat_re_frmo-green.svg";
-import master_chat from "../Images/undraw_professor_re_mj1s-green.svg";
-import crm_plus from "../Images/undraw_projections_re_ulc6-green.svg";
+import hrbg from "../Images/HR/undraw_job_offers_re_634p.svg";
+import hiring from "../Images/HR/undraw_interview_re_e5jn.svg";
+import recruitment from "../Images/HR/undraw_hiring_re_yk5n.svg"
+import onboarding from "../Images/HR/undraw_agreement_re_d4dv.svg"
+import training from "../Images/HR/undraw_progress_tracking_re_ulfg.svg"
+import joining from "../Images/HR/undraw_join_re_w1lh.svg"
+import separation from "../Images/HR/undraw_windy_day_x-63-l.svg"
 import WordPullUp from "../components/magicui/word-pull-up";
 import { FadeText } from "../components/magicui/fade-text";
 import BlurIn from "../components/magicui/blur-in";
@@ -26,60 +23,48 @@ import { BrainFlowSection } from "../components/BrainFlow";
 const sections = [
   {
     title: "Hiring Brain Flow",
-    img: charts,
+    img: hiring,
+    icon: DoorOpen,
     description:
       "Say goodbye to mundane spreadsheets or rigid systems to manage HR tasks. Get smarter and more efficient software with features designed to free you from administrative work.",
   },
   {
     title: "Recruitment Brain Flow",
-    img: flow_chart,
+    img: recruitment,
+    icon: LockOpen,
     description:
       "Say goodbye to mundane spreadsheets or rigid systems to manage HR tasks. Get smarter and more efficient software with features designed to free you from administrative work.",
   },
   {
     title: "Onboarding Brain Flow",
-    img: designation,
+    img: onboarding,
+    icon: Plus,
     description:
       "Say goodbye to mundane spreadsheets or rigid systems to manage HR tasks. Get smarter and more efficient software with features designed to free you from administrative work.",
   },
   {
     title: "Training And Evaluation Brain Flow",
-    img: tasks,
+    img: training,
+    icon: TestTube,
     description:
       "Say goodbye to mundane spreadsheets or rigid systems to manage HR tasks. Get smarter and more efficient software with features designed to free you from administrative work.",
   },
-  {
-    title: "Events",
-    img: events,
-    description:
-      "Say goodbye to mundane spreadsheets or rigid systems to manage HR tasks. Get smarter and more efficient software with features designed to free you from administrative work.",
-  },
+
   {
     title: "Joining & Handover Brainflow",
-    img: vault,
+    img: joining,
+    icon: Handshake,
     description:
       "Say goodbye to mundane spreadsheets or rigid systems to manage HR tasks. Get smarter and more efficient software with features designed to free you from administrative work.",
   },
   {
     title: "Seperating BrainFlow",
-    img: group_chat,
-    description:
-      "Say goodbye to mundane spreadsheets or rigid systems to manage HR tasks. Get smarter and more efficient software with features designed to free you from administrative work.",
-  },
-  {
-    title: "Master Chat",
-    img: master_chat,
-    description:
-      "Say goodbye to mundane spreadsheets or rigid systems to manage HR tasks. Get smarter and more efficient software with features designed to free you from administrative work.",
-  },
-  {
-    title: "CRM PLUS +",
-    img: crm_plus,
+    img: separation,
+    icon: DoorClosed,
     description:
       "Say goodbye to mundane spreadsheets or rigid systems to manage HR tasks. Get smarter and more efficient software with features designed to free you from administrative work.",
   },
 ];
-
 
 const HR = () => (
   <div className="flex flex-col gap-5 w-full min-h-screen">
@@ -99,11 +84,11 @@ const HR = () => (
           <div className="flex gap-3 flex-wrap items-center">
             <BlurIn
               word="Get Started"
-              className="bg-green-600 py-3 rounded-md flex gap-2 justify-center items-center px-8"
+              className="bg-black py-3 rounded-md flex gap-2 justify-center items-center px-8"
             />
             <BlurIn
               word="Learn More"
-              className="border border-white text-white flex justify-center gap-4 items-center py-3 rounded-md px-8"
+              className="border bg-white text-black flex justify-center gap-4 items-center py-3 rounded-md px-8"
             />
           </div>
         </div>
@@ -112,28 +97,19 @@ const HR = () => (
     </div>
 
     <div className="flex justify-center flex-col gap-8 items-center p-9">
-    <h1 className="text-center text-4xl font-bold md:text-6xl mb-5">
-          What Brain Brings To The Table
-        </h1>
+      <h1 className="text-center text-4xl font-bold md:text-6xl mb-5">
+        What Brain Brings To The Table
+      </h1>
 
-      <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4">
-        {[
-          { icon: DoorOpen, title: "Hiring Brain Flow" },
-          { icon: LockOpen, title: "Recruitment Brain Flow" },
-          { icon: Plus, title: "Onboarding Brain Flow" },
-          { icon: TestTube, title: "Training & Evaluation Brain Flow" },
-          { icon: Handshake, title: "Joining & Handover Brain Flow" },
-          { icon: DoorClosed, title: "Seperation Brain Flow" },
-        ].map(({ icon: Icon, title }) => (
+      <div className="grid md:w-[80%] lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4">
+        {sections.map((data, index) => (
           <div
-            key={title}
+            key={index}
             className="p-8 rounded-md border border-black/10 hover:-translate-y-4 hover:border-orange-700 hover:bg-gray-100 duration-200"
           >
-            <Icon size={50} className="mb-6" />
-            <h1 className="text-2xl font-bold">{title}</h1>
-            <p className="break-words max-w-[80%] mt-2">
-              All in one social media management software
-            </p>
+            <data.icon size={50} className="mb-6" />
+            <h1 className="text-2xl font-bold">{data.title}</h1>
+            <p className="break-words max-w-[80%] mt-2">{data.description}</p>
           </div>
         ))}
       </div>
